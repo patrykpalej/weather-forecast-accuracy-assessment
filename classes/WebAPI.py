5edacbe9cfbe202791d247ce31639a7ab00a2c89
Patryk Palej <patrykpalej@gmail.com>
2019-11-17 20:21:19 +0100

    Initialize WebAPI class

---
+++ classes/WebAPI.py
@@ -0,0 +1,18 @@
+"""
+Class of all possible web APIs such as DarkSky or Meteostat.
+
+Attributes:
+    - config: dictionary with configuration data for the specific API
+    - name: name of the API
+"""
+
+
+class WebAPI:
+    def __init__(self):
+        pass
+
+    def call_API(self):
+        """
+        :return: output from an API call
+        """
+        pass
