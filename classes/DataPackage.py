46919e0f1d426c830eb12ef3922c08a4980bca49
Patryk Palej <patrykpalej@gmail.com>
2019-11-17 20:26:04 +0100

    Initialize DataPackage class

---
+++ classes/DataPackage.py
@@ -0,0 +1,13 @@
+"""
+Class which represents data package acquired from an API call
+
+Attributes:
+    - raw_dict: raw dictionary created from JSON
+    - clean_dict: contains only needed data from the raw_dict
+    - API_name: name of the API from which data was acquired
+"""
+
+
+class DataPackage:
+    def __init__(self):
+        pass
