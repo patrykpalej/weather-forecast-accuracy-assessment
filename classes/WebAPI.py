3363fc5dfbd7b67b8a0fe0ff1a0be11682489223
Patryk Palej <patrykpalej@gmail.com>
2019-11-23 20:44:04 +0100

    Move call_api() method to an inheriting class
    and implement getting the data from web API

--- classes/WebAPI.py
+++ classes/WebAPI.py
@@ -1,5 +1,6 @@
 import os
 import json
+import requests
 
 
 class WebAPI:
@@ -14,18 +15,30 @@ class WebAPI:
     """
 
     def __init__(self, api_name):
-        self.name = api_name
-
         path_to_file = os.getcwd() + "/config/config_{}.json".format(api_name)
         with open(path_to_file) as handle:
             config = json.loads(handle.read())
 
         self.stations = config["stations"]
         self.key = config["key"]
-        self.api_call = config["API_call"]
+        self.api_call = config["API_call"][0]
 
-    def call_api(self):
-        """
-        :return: output from an API call
-        """
-        pass
+
+class WebAPIDarksky(WebAPI):
+    """
+    Class to handle specifically Darksky API
+    """
+
+    def __init__(self):
+        self.name = "darksky"
+        super().__init__(self.name)
+
+    def call_api(self, station_name):
+        api_call_link_formatted = self.api_call \
+            .format(self.key, self.stations[station_name]["lat"],
+                    self.stations[station_name]["lon"])
+
+        full_web_data = requests.get(api_call_link_formatted).json()
+        forecast = full_web_data["hourly"]["data"]
+
+        return forecast
