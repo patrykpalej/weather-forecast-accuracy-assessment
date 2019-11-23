ba32eaf81be05193ed5f6fb5b524fec95d7258c6
Patryk Palej <patrykpalej@gmail.com>
2019-11-23 19:43:25 +0100

    Expand WebAPI constructor and docstring

--- classes/WebAPI.py
+++ classes/WebAPI.py
@@ -7,8 +7,10 @@ class WebAPI:
     Class of all possible web APIs such as DarkSky or Meteostat.
 
     Attributes:
-        - config: dictionary with configuration data for the specific API
         - name: name of the API
+        - stations: list of dictionaries for all declared weather stations
+        - key: API access key
+        - api_call: link for the API call (with {} for parameters)
     """
 
     def __init__(self, api_name):
@@ -16,9 +18,11 @@ class WebAPI:
 
         path_to_file = os.getcwd() + "/config/config_{}.json".format(api_name)
         with open(path_to_file) as handle:
-            self.config = json.loads(handle.read())
+            config = json.loads(handle.read())
 
-        # TODO parse config and add new attributes of the class (change docstr)
+        self.stations = config["stations"]
+        self.key = config["key"]
+        self.api_call = config["API_call"]
 
     def call_api(self):
         """
