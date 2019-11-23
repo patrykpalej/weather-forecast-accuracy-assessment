b7c54d5495733435b3e08646459be712c7d35b69
Patryk Palej <patrykpalej@gmail.com>
2019-11-23 18:52:28 +0100

    Move docstrings inside the classes

--- classes/WebAPI.py
+++ classes/WebAPI.py
@@ -1,22 +1,24 @@
-"""
-Class of all possible web APIs such as DarkSky or Meteostat.
-
-Attributes:
-    - config: dictionary with configuration data for the specific API
-    - name: name of the API
-"""
-
 import os
 import json
 
 
 class WebAPI:
+    """
+    Class of all possible web APIs such as DarkSky or Meteostat.
+
+    Attributes:
+        - config: dictionary with configuration data for the specific API
+        - name: name of the API
+    """
+
     def __init__(self, api_name):
         self.name = api_name
 
         path_to_file = os.getcwd() + "/config/config_{}.json".format(api_name)
         with open(path_to_file) as handle:
             self.config = json.loads(handle.read())
+
+        # TODO parse config and add new attributes of the class (change docstr)
 
     def call_api(self):
         """
