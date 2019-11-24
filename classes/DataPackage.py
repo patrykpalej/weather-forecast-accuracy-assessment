bee8293db19b50e8a31512a5b239b85e18babe34
Patryk Palej <patrykpalej@gmail.com>
2019-11-24 12:56:18 +0100

    Create Forecast and History classes which inherit from DataPackage

--- classes/DataPackage.py
+++ classes/DataPackage.py
@@ -4,16 +4,31 @@ class DataPackage:
     a forecast or historichal data
 
     Attributes:
-        - raw_dict: raw dictionary created from JSON
+        - raw_data: raw data created from JSON
         - clean_dict: contains only needed data from the raw_dict
         - API_name: name of the API from which data was acquired
         - ?Station
     """
 
-    def __init__(self, raw_dict, station_name, api_name):
+    def __init__(self, raw_data, station_name, api_name):
+        self.raw_data = raw_data
         self.station_name = station_name
         self.api_name = api_name
-        self.raw_dict = raw_dict
 
 
+class Forecast(DataPackage):
+    """
+    Class to handle daily or hourly weather forecasts
+    """
 
+    def __init__(self, raw_data, station_name, api_name):
+        super().__init__(raw_data, station_name, api_name)
+
+
+class History(DataPackage):
+    """
+    Class to handle daily or hourly weather history
+    """
+
+    def __init__(self, raw_data, station_name, api_name):
+        super().__init__(raw_data, station_name, api_name)
