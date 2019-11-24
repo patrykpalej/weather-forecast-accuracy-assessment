01fd9759e7718c19f67d7ab908540e1bcaeb4686
Patryk Palej <patrykpalej@gmail.com>
2019-11-24 18:29:22 +0100

    Add convert_timestamps() and get_forecasted_values() methods

--- classes/DataPackage.py
+++ classes/DataPackage.py
@@ -21,6 +21,41 @@ class DataPackage:
         self.station_name = station_name
         self.api_name = api_name
 
+        self.ts_datetime, self.ts_unix, self.ts_str = self.convert_timestamps()
+        self.temperature = self.get_forecasted_values()
+
+    def convert_timestamps(self):
+        """
+        Gets timestamps from raw data and transforms it into three different
+        timestamps: datetime object, unix timestamp and string timestamp
+        """
+
+        original_timestamps = [elem["time"] for elem in self.raw_data]
+
+        if type(original_timestamps[0]) == int:
+            datetime_timestamps = [datetime.fromtimestamp(elem)
+                                   for elem in original_timestamps]
+            unix_timestamps = original_timestamps
+            string_timestamps \
+                = [datetime.fromtimestamp(elem).strftime("%Y-%m-%d %H:%M:%S")
+                   for elem in original_timestamps]
+
+        else:
+            datetime_timestamps = [parser.parse(elem)
+                                   for elem in original_timestamps]
+            unix_timestamps \
+                = [round(datetime.strptime(elem, "%Y-%m-%d %H:%M:%S")
+                         .timestamp())
+                   for elem in original_timestamps]
+            string_timestamps = original_timestamps
+
+        return datetime_timestamps, unix_timestamps, string_timestamps
+
+    def get_forecasted_values(self):
+        temperature = self.raw_data["temperature"]
+
+        return temperature
+
 
 class Forecast(DataPackage):
     """
