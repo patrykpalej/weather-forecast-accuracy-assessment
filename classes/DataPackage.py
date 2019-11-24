c6cbb8cb6142eee989758b155c062474c00a2631
Patryk Palej <patrykpalej@gmail.com>
2019-11-24 12:16:37 +0100

    Add a constructor and expand docstring of DataPackage class

--- classes/DataPackage.py
+++ classes/DataPackage.py
@@ -1,12 +1,19 @@
 class DataPackage:
     """
-    Class which represents data package acquired from an API call
+    Class which represents data package acquired from an API call. It may be
+    a forecast or historichal data
 
     Attributes:
         - raw_dict: raw dictionary created from JSON
         - clean_dict: contains only needed data from the raw_dict
         - API_name: name of the API from which data was acquired
+        - ?Station
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, raw_dict, station_name, api_name):
+        self.station_name = station_name
+        self.api_name = api_name
+        self.raw_dict = raw_dict
+
+
+
