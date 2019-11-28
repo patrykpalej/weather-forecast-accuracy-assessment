5441c6da4c381046dac23d2fa9a3854cdd2f6de6
Patryk Palej <patrykpalej@gmail.com>
2019-11-28 21:12:08 +0100

    Implement calling a forecast from today for n days

--- Get_Forecast.py
+++ Get_Forecast.py
@@ -4,12 +4,15 @@ output form
 """
 
 from classes.WebAPI import WebAPIMeteostat
+from datetime import datetime, timedelta
 
 
 meteostat_api = WebAPIMeteostat()
-forecast = meteostat_api.get_hourly_data("Warsaw", "2019-11-25",
-                                         "2019-11-30", "forecast")
 
-# print(forecast)
-# print(history.ts_str)
+forecast_start_date = datetime.now().strftime("%Y-%m-%d")
+forecast_end_date = (datetime.now() + timedelta(days=5))\
+    .strftime("%Y-%m-%d")
+
+forecast = meteostat_api.get_hourly_data("Warsaw", forecast_start_date,
+                                         forecast_end_date, "forecast")
 forecast.dump_forecast()
