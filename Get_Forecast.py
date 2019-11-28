7ddbcb8cf5ceed514dd65887788cac6892e305ad
Patryk Palej <patrykpalej@gmail.com>
2019-11-28 20:48:55 +0100

    Refresh pipeline in main script

--- Get_Forecast.py
+++ Get_Forecast.py
@@ -3,15 +3,13 @@ Calls API to obtain the forecast and exports the results to a proper
 output form
 """
 
-from classes.WebAPI import WebAPIDarksky, WebAPIMeteostat
-
-darksky_api = WebAPIDarksky()
-forecast = darksky_api.get_hourly_forecast("Warsaw")
+from classes.WebAPI import WebAPIMeteostat
 
 
 meteostat_api = WebAPIMeteostat()
-history = meteostat_api.get_hourly_history("Warsaw", "2018-01-01",
-                                           "2018-01-03")
+forecast = meteostat_api.get_hourly_data("Warsaw", "2019-11-25",
+                                         "2019-11-30", "forecast")
 
 # print(forecast)
-# print(history)
+# print(history.ts_str)
+forecast.dump_forecast()
