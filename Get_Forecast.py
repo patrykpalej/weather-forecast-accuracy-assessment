65767c7bef0419cf9f35f2f77b5840292cc5c343
Patryk Palej <patrykpalej@gmail.com>
2019-11-29 20:22:28 +0100

    Improve script for getting and saving forecasts

--- Get_Forecast.py
+++ Get_Forecast.py
@@ -10,9 +10,13 @@ from datetime import datetime, timedelta
 meteostat_api = WebAPIMeteostat()
 
 forecast_start_date = datetime.now().strftime("%Y-%m-%d")
-forecast_end_date = (datetime.now() + timedelta(days=5))\
+forecast_end_date = (datetime.now() + timedelta(days=10))\
     .strftime("%Y-%m-%d")
 
-forecast = meteostat_api.get_hourly_data("Warsaw", forecast_start_date,
-                                         forecast_end_date, "forecast")
-forecast.dump_forecast()
+cities = ["Warsaw", "Rome", "London"]
+
+for city in cities:
+    forecast = meteostat_api.get_hourly_data(city, forecast_start_date,
+                                             forecast_end_date, "forecast")
+
+    forecast.dump_forecast(forecast_start_date + " ~ " + forecast_end_date)
