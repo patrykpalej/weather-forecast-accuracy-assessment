ecb30416a64b1a26eec89f58d5c9b186dab7f527
Patryk Palej <patrykpalej@gmail.com>
2019-12-06 17:19:55 +0100

    Add cut-off in Get_Forecast script

--- Get_Forecast.py
+++ Get_Forecast.py
@@ -9,14 +9,16 @@ from datetime import datetime, timedelta
 
 meteostat_api = WebAPIMeteostat()
 
+n_of_days = 10
 forecast_start_date = datetime.now().strftime("%Y-%m-%d")
-forecast_end_date = (datetime.now() + timedelta(days=10))\
+forecast_end_date = (datetime.now() + timedelta(days=n_of_days))\
     .strftime("%Y-%m-%d")
 
 cities = ["Warsaw", "Rome", "London", "Moscow"]
 
 for city in cities:
     forecast = meteostat_api.get_hourly_data(city, forecast_start_date,
-                                             forecast_end_date, "forecast")
+                                             forecast_end_date, "forecast",
+                                             cut_off=n_of_days*24)
 
-    forecast.dump_forecast(forecast_start_date + " ~ " + forecast_end_date)
+    forecast.dump_forecast()
