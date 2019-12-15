e7bdf4c0d60468e9aa6ba3466e0079c691a71f63
Patryk Palej <patrykpalej@gmail.com>
2019-12-15 09:26:14 +0100

    Add New York and Novosibirsk to Get_Forecast

--- Get_Forecast.py
+++ Get_Forecast.py
@@ -14,7 +14,7 @@ forecast_start_date = datetime.now().strftime("%Y-%m-%d")
 forecast_end_date = (datetime.now() + timedelta(days=n_of_days))\
     .strftime("%Y-%m-%d")
 
-cities = ["Warsaw", "Rome", "London", "Moscow"]
+cities = ["Warsaw", "Rome", "London", "Moscow", "New York", "Novosibirsk"]
 
 for city in cities:
     forecast = meteostat_api.get_hourly_data(city, forecast_start_date,
