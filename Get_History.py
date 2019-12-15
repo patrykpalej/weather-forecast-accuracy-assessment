6c046bce712a4c5ebb7b0ce674cfe0e5e1e194da
Patryk Palej <patrykpalej@gmail.com>
2019-12-15 09:42:13 +0100

    Get end_date of the history range from now()

--- Get_History.py
+++ Get_History.py
@@ -3,16 +3,19 @@ Calls API to obtain the history and exports the results to a proper
 output form
 """
 import os
+from datetime import datetime
 
 from classes.WebAPI import WebAPIMeteostat
-from functions.get_forecast_period import get_forecast_period
+from functions.get_forecast_period import get_forecast_start_date
 
 
 meteostat_api = WebAPIMeteostat()
 
 path_to_data = os.getcwd() + "/data/"
-history_start_date, history_end_date = get_forecast_period(path_to_data,
-                                                           "London")
+history_start_date = get_forecast_start_date(path_to_data,
+                                             "London")
+history_end_date = "{}-{}-{}".\
+    format(datetime.now().year, datetime.now().month, datetime.now().day)
 
 cities = ["Warsaw", "Rome", "London", "Moscow", "New York", "Novosibirsk"]
 
--- functions/get_forecast_period.py
+++ functions/get_forecast_period.py
@@ -3,9 +3,9 @@ import json
 from datetime import datetime
 
 
-def get_forecast_period(path_to_data, city_name):
+def get_forecast_start_date(path_to_data, city_name):
     """
-    Gets first and last timestamp from forecasts for a given city
+    Gets first timestamp from forecasts for a given city
     """
     directory_to_search = path_to_data + "/forecasts/" + city_name
 
@@ -13,24 +13,15 @@ def get_forecast_period(path_to_data, city_name):
                   for file_name in list(os.walk(directory_to_search))[0][2]]
 
     start_dates = list()
-    end_dates = list()
 
     for file_path in json_files:
         with open(file_path, 'r') as f:
             forecast_data = json.load(f)
             start_dates.append(
                 datetime.fromtimestamp(forecast_data["unix_timestamp"][0]))
-            end_dates.append(
-                datetime.fromtimestamp(forecast_data["unix_timestamp"][-1]))
 
-    # print("start for all files: ", start_dates)
-    # print("end for all files: ", end_dates)
-    #
-    # print("total start: ", min(start_dates))
-    # print("total end: ", max(end_dates))
+    start_date = min(start_dates)
+    start_date_str \
+        = start_date.strftime("%Y-%m-%d")
 
-    start_date, end_date = min(start_dates), max(end_dates)
-    start_date_str, end_date_str \
-        = start_date.strftime("%Y-%m-%d"), end_date.strftime("%Y-%m-%d")
-
-    return start_date_str, end_date_str
+    return start_date_str
