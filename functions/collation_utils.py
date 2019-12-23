91aea9a657401d75b3b50dc31adc7d99bde4c0c8
Patryk Palej <patrykpalej@gmail.com>
2019-12-23 22:47:55 +0100

    Initialize collation_utils file
    Functions used in Make_Collation are there

---
+++ functions/collation_utils.py
@@ -0,0 +1,17 @@
+def get_history_subset(location, start_timestamp, end_timestamp):
+    """
+    Takes location, start and end timestamps. Returns a dictionary which is
+    a subset
+    """
+
+    return 1
+
+
+def collate_dicts(forecast_dict, history_dict):
+    """
+    Takes forecast and history dictionaries. Returns a dataframe which is a
+    collation of them
+    """
+
+    return 1
+
