d73f2f3572fc2ee332448e4777cf5f326cf58848
Patryk Palej <patrykpalej@gmail.com>
2019-12-26 21:40:29 +0100

    Add many things to Make_Collation.py
    Preparing collations in .csv is now implemented

--- Make_Collation.py
+++ Make_Collation.py
@@ -4,11 +4,16 @@ collation of historichal data and the forecast
 """
 import os
 import json
+import pandas as pd
 
 from getHistory import get_history
 from functions.get_forecast_period import get_forecast_limits
-from functions.collation_utils import get_history_subset, collate_dicts
+from functions.collation_utils import clean_subfolders, get_history_subset, \
+    shorten_forecast_dict, collate_dicts
 
+
+clean_subfolders(os.getcwd() + "/data/history/")
+clean_subfolders(os.getcwd() + "/data/collations/")
 
 locations = ["Warsaw", "Rome", "London", "Moscow", "New York", "Novosibirsk"]
 for location in locations:
@@ -17,19 +22,28 @@ for location in locations:
 
 for location in locations:
     forecast_dir = os.getcwd() + "/data/forecasts/" + location
-    json_files = [forecast_dir + "/" + file_name
-                  for file_name in list(os.walk(forecast_dir))[0][2]]
+    forecast_jsons = [forecast_dir + "/" + file_name
+                      for file_name in os.listdir(forecast_dir)]
 
-    for file in json_files:
+    collation_dfs = []
+    for file in forecast_jsons:
         with open(file) as f:
             forecast_dict = json.loads(f.read())
 
         start_timestamp, end_timestamp = get_forecast_limits(forecast_dict)
 
-        history_dict = get_history_subset(location, start_timestamp,
+        history_dir = os.getcwd() + "/data/history/" + location
+        history_path = history_dir + "/" + os.listdir(history_dir)[0]
+        history_dict = get_history_subset(history_path, start_timestamp,
                                           end_timestamp)
 
-        collation_df = collate_dicts(forecast_dict, history_dict)
+        if len(forecast_dict["timestamp"]) > len(history_dict["timestamp"]):
+            forecast_dict = shorten_forecast_dict(forecast_dict, history_dict)
 
-        # dump collation_df to a csv in data/collations
-        # maybe whole collation for one location for the whole period in one df
+        collation_dfs.append(collate_dicts(forecast_dict, history_dict))
+
+    overall_collation_df = pd.concat(collation_dfs, ignore_index=True)
+    collation_file_name = os.listdir(history_dir)[0]
+    overall_collation_df.to_csv(os.getcwd() + "/data/collations/" + location
+                                + "/" + collation_file_name[:-5] + ".csv",
+                                sep='\t')
