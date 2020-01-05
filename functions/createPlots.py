42053b52c48362941acff6a5540b276e0f2826b4
Patryk Palej <patrykpalej@gmail.com>
2020-01-05 11:33:25 +0100

    Initialize createPlots file with plotting functions

---
+++ functions/createPlots.py
@@ -0,0 +1,27 @@
+"""
+This file contains functions which generate subsequent types of plots
+"""
+
+
+def forecast_vs_actual_separate(collations_dict, period):
+    pass
+
+
+def forecast_vs_actual_averaged(collations_dict, period):
+    pass
+
+
+def r2_vs_time_separate(collations_dict, period):
+    pass
+
+
+def r2_vs_time_averaged(collations_dict, period):
+    pass
+
+
+def r2_vs_location(collations_dict, period):
+    pass
+
+
+def r2_vs_location_and_time(collations_dict, period):
+    pass
