2f2186194a8a8adede9cf33cd9df4c7839166376
Patryk Palej <patrykpalej@gmail.com>
2020-01-10 20:08:44 +0100

    Add some changes to makeCollation after making it a function

--- makeCollation.py
+++ makeCollation.py
@@ -8,44 +8,37 @@ import pandas as pd
 
 from getHistory import get_history
 from functions.getForecastPeriod import get_forecast_limits
-from functions.collationUtils import clean_subfolders, get_history_subset, \
+from functions.collationUtils import get_history_subset, \
     shorten_forecast_dict, collate_dicts
 
 
-def make_collation():
-    clean_subfolders(os.getcwd() + "/data/history/")
-    clean_subfolders(os.getcwd() + "/data/collations/")
+def make_collation(location):
 
-    locations = os.listdir(os.getcwd() + "/data/forecasts")
+    get_history(location)
 
-    for location in locations:
-        get_history(location)
+    forecast_dir = os.getcwd() + "/data/forecasts/" + location
+    forecast_jsons = [forecast_dir + "/" + file_name
+                      for file_name in os.listdir(forecast_dir)]
 
+    collation_dfs = []
+    history_dir = os.getcwd() + "/data/history/" + location
+    for file in forecast_jsons:
+        with open(file) as f:
+            forecast_dict = json.loads(f.read())
 
-    for location in locations:
-        forecast_dir = os.getcwd() + "/data/forecasts/" + location
-        forecast_jsons = [forecast_dir + "/" + file_name
-                          for file_name in os.listdir(forecast_dir)]
+        start_timestamp, end_timestamp = get_forecast_limits(forecast_dict)
 
-        collation_dfs = []
-        history_dir = os.getcwd() + "/data/history/" + location
-        for file in forecast_jsons:
-            with open(file) as f:
-                forecast_dict = json.loads(f.read())
+        history_path = history_dir + "/" + os.listdir(history_dir)[0]
+        history_dict = get_history_subset(history_path, start_timestamp,
+                                          end_timestamp)
 
-            start_timestamp, end_timestamp = get_forecast_limits(forecast_dict)
+        if len(forecast_dict["timestamp"]) > len(history_dict["timestamp"]):
+            forecast_dict = shorten_forecast_dict(forecast_dict, history_dict)
 
-            history_path = history_dir + "/" + os.listdir(history_dir)[0]
-            history_dict = get_history_subset(history_path, start_timestamp,
-                                              end_timestamp)
+        collation_dfs.append(collate_dicts(forecast_dict, history_dict))
 
-            if len(forecast_dict["timestamp"]) > len(history_dict["timestamp"]):
-                forecast_dict = shorten_forecast_dict(forecast_dict, history_dict)
-
-            collation_dfs.append(collate_dicts(forecast_dict, history_dict))
-
-        overall_collation_df = pd.concat(collation_dfs, ignore_index=True)
-        collation_file_name = os.listdir(history_dir)[0]
-        overall_collation_df.to_csv(os.getcwd() + "/data/collations/" + location
-                                    + "/" + collation_file_name[:-5] + ".csv",
-                                    sep='\t')
+    overall_collation_df = pd.concat(collation_dfs, ignore_index=True)
+    collation_file_name = os.listdir(history_dir)[0]
+    overall_collation_df.to_csv(os.getcwd() + "/data/collations/" + location
+                                + "/" + collation_file_name[:-5] + ".csv",
+                                sep='\t')
